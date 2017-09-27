1c63a6bb5b781ba1fece3677870898a8c49a5d3d
Joaquin Kunkel <jek537@nyu.edu>
2017-09-27 18:48:53 +0400

    Added several features

--- public/script.js
+++ public/script.js
@@ -3,6 +3,36 @@ var our_button = document.getElementById("gobutton");
 
 our_button.addEventListener('click', function(){
   console.log("i am clicked");
+  $("header").animate({margin: "0 auto"}, {duration: 200});
+  $("video").css("visibility", "hidden");
+  $(".contents").css("background", "linear-gradient(to bottom, #0f7fff 0%,#00c7ff 100%)");
+  $("header").attr("class", "row");
+  $("header").css("display", "block");
+  $("#title").attr("class", "col-sm-4");
+  $("form").attr("class", "col-sm-5");
+  $("form").css("margin", "0 5px");
+  $("header").css("padding-bottom", "30px");
+  $("h1").css("margin", "0 5px");
+  $(".msb").css("margin", ".67em 10px");
+  $("header").css("justify-content", "center");
+
+//Parse the JSON file to import the data into this D3 script
+  var data = [];
+  d3.json("../data/places_dummy.json", function(error, data) {
+
+    d3.select("body").select(".contents").select(".data_display").selectAll("h1")
+      .data(data.all_places)
+      .enter()
+      .append("h1")
+      .text(function(d){
+        var input_text = $('input').val();
+        if(input_text == d.name){
+          return d.name + JSON.stringify(d.logs);
+        }
+      });
+
+    });
+
 });
 
 //An "error" or "fail" function
