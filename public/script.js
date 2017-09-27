de93e0886a60c0f52279c6ffb2c2143085eb3b07
Joaquin Kunkel <jek537@nyu.edu>
2017-09-28 00:36:38 +0400

    Further D3 features

--- public/script.js
+++ public/script.js
@@ -9,12 +9,7 @@ function itWorked(data){
 	console.log("Worked !");
 
 	var temp_info = JSON.parse(data);
-	console.log(temp_info);
 	visualize(data);
-
-	//var htmlString = '';
-	//htmlString += '<p> Most recently, people have reported that ' + temp_info.place + ' is level ' + temp_info.logs.temp + ' cold</p>';
-	//$(htmlString).appendTo('.data_display');
 }
 
 
@@ -38,85 +33,125 @@ function visualize(dataset){
 	var all_logs = data.logs;
 	var logs = [];
 
-	var temps = ["", "Freezing", "Cold", "Cool", "Just right", "Warm", "Hot"];
-	//Take only 7 elements from all_logs.
+	var temps = ["", "freezing", "cold", "cool", "just right", "warm", "hot"];
 
-	function seven_logs(logs){
-		var my_logs = []
-		for(i = 0; i < 7; i++){
-			my_logs.push(all_logs[i]);
-		}
-		return my_logs;
-	};
-
-	logs = all_logs.length <= 7 ? all_logs: seven_logs(all_logs);
-
-	console.log(logs);
-
+	//logs = all_logs.length <= 7 ? all_logs: seven_logs(all_logs);
 	var current_date = new Date();
 	var temp_avg = 0; //Compute the average temperatures of a place.
 	var temp_count = 0;
 	var split_date = [];
+	var day_avg;
+	var temp_count_2 = 0
+	var new_log = {};
+	var already_added = [];
+	var l = {};
+	var flag = 0;
+	todays_month = current_date.getMonth();
+	todays_day = current_date.getDate();
+
+	for(e = 0; e < all_logs.length; e++){
+		flag = 0
+
+		l = all_logs[e];
+		new_log = {};
+		split_date = l.date.split("_");
+		if(todays_month == split_date[0] && todays_day - split_date[1] < 7){
+
+			for(i = 0; i < already_added.length; i++){
+				if(all_logs[e].date == already_added[i]){
+					flag = 1;
+					break;
+				}
+			}
+
+			if(!flag){
+
+				//Compute the average of all temperatures for that date.
+				day_avg = 0;
+				temp_count_2 = 0;
+				all_logs.forEach(function(my_log){
+					if(my_log.date == l.date){
+						console.log(all_logs[e].date);
+						day_avg += parseInt(my_log.temp);
+						temp_count_2++;
+					}
+				});
+				day_avg = day_avg/temp_count_2;
+
+				//Add this average to our logs array.
+				new_log.date = l.date;
+				new_log.temp = day_avg;
+				logs.push(new_log);
+
+				already_added.push(l.date);
+			}
+
+		}
+	}
+
+	function todays_avg(){
+		for(i = 0; i < logs.length; i++){
+			split_date = logs[i].date.split("_");
+			console.log(logs[i]);
+			if(split_date[0] == todays_month && split_date[1] == todays_day){
+				return logs[i].temp;
+			}
+		}
+		return 0;
+	}
+
+	console.log(logs);
+
 
 	logs.forEach(function(log){
-		split_date = log.date.split("_");
-		if(current_date.getMonth() == split_date[0] && current_date.getDate() - split_date[1] < 7){
-			console.log(temp_avg);
-			temp_avg += parseInt(log.temp);
-
-			temp_count++;
-		}
+		temp_avg += parseInt(log.temp);
 	});
+	temp_avg = temp_avg/logs.length;
 
-	temp_avg = temp_avg/temp_count;
-
-	//console.log(temp_avg, "   ", temp_count);
-	//console.log(place_name);
-
-	var left_limit = data.logs[data.logs.length-1].date.split("_"); //The oldest date of the place's logs.
-	var right_limit = data.logs[0].date.split("_"); //The most recent date of the place's logs.
 
 	var w = $(window).width() <= 500 ? $(window).width() * 0.9 : 500;
 	var h = 100;
 	var barPadding = 1;
 
-	//An array that will store [[date, temp], [date, temp], ...]:
-
 	var display = d3.select("body").select(".contents").select(".data_display");
+
+	display.selectAll("svg").remove();
+	display.selectAll("h2").remove();
+	display.selectAll("p").remove();
 
 	var div = display.append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
 	display.append("h2")
-		.text(place_name)
+		.text(function(d){
+			if(todays_avg()){
+				return(place_name + " is " + temps[Math.floor(todays_avg())] + " today.");
+			}
+			else{
+				return(place_name);
+			}
+		})
 		.style("color", "white");
 
 	display.append("p")
 		.text(function(d){
-			return("Over the past week, the temperature at "+ place_name + " has been reported to be " + temps[Math.floor(temp_avg)] + " on average.");
-		});
-			/*
-	display.selectAll("p")
-		.data(data.logs)
-		.enter()
-		.append("p")
-		.text(function(d){
-			return("Date: " + d.date + "\t" + "Temperature: " + d.temp);
+			return("Over the past week, the temperature at "+ place_name + " has been reported to be " + temps[Math.floor(temp_avg)] + " on average. Data for the past 7 days:");
 		})
 		.style("color", "white");
-*/
+
 
 	var svg = display.append("svg")
 										.attr("width", w)
 										.attr("height", h);
+
 
 	svg.selectAll("rect")
 			.data(logs)
 			.enter()
 			.append("rect")
 			.attr("x", function(d, i){
-				return i * (w / logs.length);
+				return (logs.length-i-1) * (w / logs.length);
 			})
 			.attr("y", function(d, i){
 				return h-d.temp *10;
